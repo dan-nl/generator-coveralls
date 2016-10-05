08275e32154b01e053394451f45ba4191423f5b4
dan-nl <d_entous@yahoo.com>
2016-10-05 17:57:07 +0200

    re-worked prompting

---
+++ app/helpers/prompt-install-coveralls.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @type {*[]}
+ */
+var prompts = [
+  {
+    default: true,
+    message: 'install coveralls',
+    name: 'install-coveralls',
+    type: 'confirm'
+  }
+];
+
+module.exports = prompts;
